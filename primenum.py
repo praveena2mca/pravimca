6030e5d6e4ffa93d958005544b42ecfce3ab14f3
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-03-15 07:52:41 +0530

    primenum.py

---
+++ primenum.py
@@ -0,0 +1,10 @@
+x=int(input("enter the number:"))
+if(a>1):
+    for i in range(2,n):
+        if(a%i==0):
+            print("not a prime number"))
+            print(i,"times",a//i,"is",a)
+        else:
+            print("prime number")
+else:
+    print("not a prime number")
