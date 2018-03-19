ff49e5117adcb065bd55120ff93a0cc0e66d075e
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-03-19 20:55:58 +0530

    listinnum.py

---
+++ listinnum.py
@@ -0,0 +1,10 @@
+t=[11345,345,1,2,4,5]
+n=int(input("enter the number"))
+found=false
+for i in range(len(t):
+    if(t[i]==u)"
+        found=True
+        print("%d is found %dposition",(ui))
+        break
+if(found==False):
+    print("%d is not in %list",%u)
