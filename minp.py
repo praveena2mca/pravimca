ef2a8f51285b02ae07b811cdae636353c68ae940
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-04-21 15:49:12 +0530

    minp.py

---
+++ minp.py
@@ -0,0 +1,14 @@
+x=int(input())
+y=int(input())
+l=[]
+l1=[]
+for i in range(x):
+    a=int(input())
+    l.append(a)
+for j in range(y):
+    c=0
+    u=int(input())
+    v=int(input())
+    for i in range(u,v):
+        l1.append(l)
+    print(min(l))
