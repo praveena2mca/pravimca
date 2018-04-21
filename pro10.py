4d1bdbfc4ef9d5cacead56b6729ec656fcd8310a
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-04-21 15:18:33 +0530

    pro10.py

---
+++ pro10.py
@@ -0,0 +1,12 @@
+def pro10(s1, s2):
+  s1 = input("enter the first letter")
+  s2 = input("enter the Second letter")
+  if s1 != s2:
+      print ("yes")
+  else:
+      print ("No")
+  return pro10(s1,s2)
+  print (pro10(s1,s2))
+s1 = ()
+s2 = ()
+print, pro10(s1, s2)
