4fb31b86d356c291b0dc22e611b5c15b0fbaf454
praveena2mca <36407730+praveena2mca@users.noreply.github.com>
2018-04-21 12:32:03 +0530

    hunter2.py

---
+++ hunter2.py
@@ -0,0 +1,28 @@
+def fun(l):
+	l.sort(reverse=True)
+	x=''
+	y=0
+	new=''
+	for i in l:
+		x+=str(i)
+		y+=1
+	print(x)
+	y=0
+	for i in range(len(x)-1,-1,-1):
+		if y==3:
+			y=0
+			new+=','
+		new+=x[i]
+		y=y+1
+	print(new[::-1])
+	
+def main():
+	try:
+		n=int(input())
+		l=[]
+		for i in range(n):
+			l.append(int(input()))
+		fun(l)
+	except:
+		print('invalid')
+main()
